8bfe876ea1e04e90a711e7d0043e5ebf8f626e34
karthikdebug <140650319+karthikdebug@users.noreply.github.com>
2023-07-27 01:29:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function imagegen() {
+    var image = document.createElement('img');
+    var div = document.getElementById('imagecontainer');
+    image.src = "https://res.cloudinary.com/dtyu8gwf9/image/upload/v1689095032/message_esepnf.png";
+    div.appendChild(image);
+}
